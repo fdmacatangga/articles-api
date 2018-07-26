46eba3f0d365621a6cfbf2c6072cb9e8cf7585ba
Franz Darelle <franzmacatangga2@gmail.com>
2018-07-27 05:36:51 +0800

    initial commit

---
+++ resources/assets/js/components/navbar.vue
@@ -0,0 +1,7 @@
+<template>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-info mb-2">
+        <div class="contrainer">
+            <a href="#" class="navbar-brand ml-3">Articles</a>
+        </div>
+    </nav>
+</template>
